106ee6bb136ccd010afb419f623dee0240882433
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-09-09 12:02:42 +1000

    feat: basic layout and components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   content: [ './src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}' ],
   plugins: [ require('daisyui') ],
+  daisyui: {
+    themes: [ "dracula", "valentine" ],
+    darkTheme: "dracula",
+    prefix: "",
+    logs: true,
+    utils: true,
+    styled: true,
+    base: true
+  }
 }
