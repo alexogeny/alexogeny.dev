d546ef932e002564a4f9103a1a257b4e642cf362
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-09-09 22:17:46 +1000

    refactor: clone astrowind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,89 @@
-import netlify from '@astrojs/netlify/functions'
-import { defineConfig } from 'astro/config'
+import path from "path";
+import { fileURLToPath } from "url";
 
-import { remarkReadingTime } from './src/remark-plugins/reading-time.mjs'
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import tasks from "./src/utils/tasks";
+
+import { readingTimeRemarkPlugin } from "./src/utils/frontmatter.mjs";
+
+import { ANALYTICS, SITE } from "./src/utils/config.ts";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const whenExternalScripts = (items = []) =>
+  ANALYTICS.vendors.googleAnalytics.id &&
+  ANALYTICS.vendors.googleAnalytics.partytown
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  site: SITE.site,
+  base: SITE.base,
+  trailingSlash: SITE.trailingSlash ? "always" : "never",
+
+  output: "static",
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ["*"],
+        "flat-color-icons": [
+          "template",
+          "gallery",
+          "approval",
+          "document",
+          "advertising",
+          "currency-exchange",
+          "voice-presentation",
+          "business-contact",
+          "database",
+        ],
+      },
+    }),
+
+    ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ["dataLayer.push"] },
+      })
+    ),
+
+    tasks(),
+
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+    }),
+  ],
+
   markdown: {
-    remarkPlugins: [ remarkReadingTime ]
-  }
-})
+    remarkPlugins: [readingTimeRemarkPlugin],
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+});
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,68 @@
-import { defineCollection, reference, z } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 
-const posts = defineCollection({
-  type: 'content',
+const metadataDefinition = () =>
+  z
+    .object({
+      title: z.string().optional(),
+      ignoreTitleTemplate: z.boolean().optional(),
+
+      canonical: z.string().url().optional(),
+
+      robots: z
+        .object({
+          index: z.boolean().optional(),
+          follow: z.boolean().optional(),
+        })
+        .optional(),
+
+      description: z.string().optional(),
+
+      openGraph: z
+        .object({
+          url: z.string().optional(),
+          siteName: z.string().optional(),
+          images: z
+            .array(
+              z.object({
+                url: z.string(),
+                width: z.number().optional(),
+                height: z.number().optional(),
+              })
+            )
+            .optional(),
+          locale: z.string().optional(),
+          type: z.string().optional(),
+        })
+        .optional(),
+
+      twitter: z
+        .object({
+          handle: z.string().optional(),
+          site: z.string().optional(),
+          cardType: z.string().optional(),
+        })
+        .optional(),
+    })
+    .optional();
+
+const postCollection = defineCollection({
   schema: z.object({
-    title: z.string(),
+    publishDate: z.date().optional(),
+    updateDate: z.date().optional(),
     draft: z.boolean().optional(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string()
-    }).optional(),
-    tags: z.array(z.string()),
-    date: z.string(),
-    snippet: z.string(),
-    author: reference('authors')
+
+    title: z.string(),
+    excerpt: z.string().optional(),
+    image: z.string().optional(),
+
+    category: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    author: z.string().optional(),
+
+    metadata: metadataDefinition(),
   }),
 });
 
-const authors = defineCollection({
-  type: 'data',
-  schema: z.object({
-    name: z.string(),
-    avatar: z.string(),
-    bio: z.string(),
-    socials: z.array(z.object({
-      name: z.string(),
-      url: z.string(),
-    })),
-    website: z.string(),
-  }),
-})
-
 export const collections = {
-  posts, authors
+  post: postCollection,
 };
