0821dd3690c3b2c8dd8d77c43a43b9016b7e812e
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-09-09 21:55:53 +1000

    chore: drop daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  content: [ './src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}' ],
-  plugins: [ require('daisyui') ],
-  daisyui: {
-    themes: [ "dracula", "valentine" ],
-    darkTheme: "dracula",
-    prefix: "",
-    logs: true,
-    utils: true,
-    styled: true,
-    base: true
-  }
-}
+  content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
+};
