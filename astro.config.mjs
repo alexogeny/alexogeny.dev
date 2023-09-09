17dd76a42770356122f9ce4f5217f5e2ed657c1c
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-09-09 13:35:37 +1000

    feat: reading time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import netlify from '@astrojs/netlify/functions'
 import { defineConfig } from 'astro/config'
 
+import { remarkReadingTime } from './src/remark-plugins/reading-time.mjs'
+
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  markdown: {
+    remarkPlugins: [ remarkReadingTime ]
+  }
 })
